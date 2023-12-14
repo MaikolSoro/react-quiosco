130a9917686a76452c61c69182cdad200a751cc4
Maikol <maikolsoro.z1998@gmail.com>
2023-12-13 19:50:24 -0800

    Creating the Authentication Layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
 
 export default function AuthLayout() {
   return (
-    <div>AuthLayout</div>
+    <div>
+      AuthLayout
+      <Outlet />
+    </div>
   )
 }
