8bdd4b7125d2cde33d443b85e75bdd442bcd106a
Maikol <maikolsoro.z1998@gmail.com>
2023-12-14 14:36:06 -0800

    Obtaining and Displaying Categories

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { categorias } from "../data/Categories" 
+
 
 export default function Sidebar() {
   return (
-    <div className="w-72">Sidebar</div>
+    <div className="md:w-72">
+        <div className="p-4">
+            <img 
+               className="w-40"
+               src="img/logo.svg"
+            />
+        </div>
+
+        <div className="mt-10">
+            {categorias.map( category => (
+                <p>{categorias.nombre}</p>
+            ))}
+        </div>
+    </div>
   )
 }
