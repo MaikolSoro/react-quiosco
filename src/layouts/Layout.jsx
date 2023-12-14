b0d8081ded815b246a54d178c7694c39ceeb00c7
Maikol <maikolsoro.z1998@gmail.com>
2023-12-14 14:27:20 -0800

    Creating the Main Layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,9 +1,16 @@
 import { Outlet } from 'react-router-dom'
+import Sidebar from '../components/Sidebar'
+import Resumen from '../components/Resumen'
 
 export default function Layout() {
   return (
-    <div>
-      <Outlet />
+    <div className='md:flex'>
+        <Sidebar />
+
+        <main className='flex-1'>
+          <Outlet />
+        </main>
+        <Resumen />
     </div>
   )
 }
