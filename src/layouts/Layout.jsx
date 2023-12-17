d877c12b1139d744bdf6c92de001170a111bc239
Maikol <maikolsoro.z1998@gmail.com>
2023-12-16 16:40:13 -0800

    Creating modal products

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,16 +1,42 @@
 import { Outlet } from 'react-router-dom'
+import Modal from 'react-modal'
 import Sidebar from '../components/Sidebar'
 import Resumen from '../components/Resumen'
 
-export default function Layout() {
-  return (
-    <div className='md:flex'>
-        <Sidebar />
+import useQuisco from '../hooks/useQuiosco'
 
-        <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-3'>
-          <Outlet />
-        </main>
-        <Resumen />
-    </div>
+import ModalProduct from '../components/ModalProduct'
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement('#root')
+export default function Layout() {
+
+  const { modal } = useQuisco();
+  
+  return (
+    <>
+      <div className='md:flex'>
+          <Sidebar />
+
+          <main className='flex-1 h-screen overflow-y-scroll bg-gray-100 p-3'>
+            <Outlet />
+          </main>
+          <Resumen />
+      </div>
+
+        <Modal isOpen={modal} style={customStyles}>
+          <ModalProduct />
+        </Modal>
+    </>
   )
 }
