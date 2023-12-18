2117da309dc01ad5eba4a076c5a0be9477d5baac
Maikol <maikolsoro.z1998@gmail.com>
2023-12-18 10:33:01 -0800

    Disable Checkout Button if the Order is empty

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -6,6 +6,8 @@ import ProductSummary from './ProductSummary';
 export default function Resumen() {
   const { order, total } = useQuiosco();
 
+  const checkOrder = () => order.length === 0;
+
   return (
     <aside className="w-72 h-screen overflow-y-scroll p-5">
       <h1 className="text-4xl font-black">
@@ -38,8 +40,9 @@ export default function Resumen() {
         <div className="mt-5">
           <input 
              type="submit"
-             className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer"
+             className={`${checkOrder() ? 'bg-indigo-100' : ' bg-indigo-600 hover:bg-indigo-800' } px-5 py-2 rounded uppercase font-bold text-white text-center w-full cursor-pointer`}
              value="Confirmar Pedido"
+             disabled={checkOrder()}
           />
         </div>
       </form>
